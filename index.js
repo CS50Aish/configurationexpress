0e643d2a89b87e33dc5a7e710cae2ada69aa4169
Ubuntu <labuser@ubuntu2204.sbder3d3inmu3lotudpaoql22a.rx.internal.cloudapp.net>
2024-07-13 12:00:37 +0000

    Updated Express Configuration Project Files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 var express = require("express");
+var app = express();
+
+// Use app.get() and app.set()
+console.log(app.get('title'));
+console.log(app.set('title', 'Configuration Commands'));
+console.log(app.get('title'));
+
+// Use app.enable and app.disable
+app.enable('Configuration command - enable function');
+console.log(app.get('Configuration command - enable function'));
+app.disable('Configuration command - disable function');
+console.log("Disable function => " + app.get('Configuration command - disable function'));
+
+// Use app.enabled and app.disabled
